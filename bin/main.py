31d8f9992847d836e619749496d07849caf861f2
santiTFF <santiago.collomb@thefuzzyfish.com>
2022-08-11 09:33:56 -0300

    git-token

--- bin/main.py
+++ bin/main.py
@@ -1,21 +1,24 @@
 import optparse
+import sys
 from asserts import assert_argument_ammount, assert_projectName
 from rebaseBranches import rebase_branches
 
 
 def main():
-    parser = optparse.OptionParser()
-    parser.set_defaults(debug=False, xls=False)
-    (options, args) = parser.parse_args()
+    try:
+        parser = optparse.OptionParser()
+        parser.set_defaults(debug=False, xls=False)
+        (options, args) = parser.parse_args()
 
-    assert_argument_ammount(args)
+        assert_argument_ammount(args)
 
-    project_name = args[0]
-    branches = args[1].split(',')
+        project_name = args[0]
+        branches = args[1].split(',')
 
-    assert_projectName(project_name)
+        assert_projectName(project_name)
 
-    rebase_branches(project_name, branches, True)
-
+        rebase_branches(project_name, branches, True)
+    except Exception as e:
+        print(str(e), file=sys.stderr)
 
 main()
