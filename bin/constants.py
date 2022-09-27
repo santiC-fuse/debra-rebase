030e4c2a12e9a66c7061a896fa3c03e3cc3376e5
santiTFF <santiago.collomb@thefuzzyfish.com>
2022-09-27 15:30:19 -0300

    spores-migration

--- bin/constants.py
+++ bin/constants.py
@@ -1,2 +1,2 @@
 VALID_TOKENS=['Token jo974huejtb4zfyjmj8dku5per', 'Token odb9yc1c6fgntx6oygsi7fhnya', 'Token IBNviLag6KgtbQ6m0rK2WCik', 'Token 1ip0owCTnJVM3YedGFYJ2TIA']
-VALID_PROJECT_NAMES=['upfront-backend', 'upfront-frontend']
+VALID_PROJECT_NAMES=['spores-backend', 'spores-frontend']
